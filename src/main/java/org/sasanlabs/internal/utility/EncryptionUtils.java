4b9fc2b8e9d15cacb8f817c6ef1f4f922233a741
atiwari80 <ayush.tiwari2@mphasis.com>
2026-06-24 17:02:09 -0400

    fix: remediate vulnerability in src/main/java/org/sasanlabs/internal/utility/EncryptionUtils.java

--- src/main/java/org/sasanlabs/internal/utility/EncryptionUtils.java
+++ src/main/java/org/sasanlabs/internal/utility/EncryptionUtils.java
@@ -1,1 +1,17 @@
-Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+Cipher cipher = Cipher.getInstance("AES/GCM/NoPADDING");
+
+// Added import
+import javax.crypto.spec.GCMParameterSpec;
+
+// Changed code from ECB to GCM
+public byte[] encrypt(byte[] plaintext, byte[] iv) {
+    GCMParameterSpec spec = new GCMParameterSpec(128, iv);
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey, spec);
+    return cipher.doFinal(plaintext);
+}
+
+public byte[] decrypt(byte[] ciphertext, byte[] iv) {
+    GCMParameterSpec spec = new GCMParameterSpec(128, iv);
+    cipher.init(Cipher.DECRYPT_MODE, secretKey, spec);
+    return cipher.doFinal(ciphertext);
+}
